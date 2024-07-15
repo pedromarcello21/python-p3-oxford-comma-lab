531b81ec7f733235bc8f5c2de3627c405460725a
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-07-15 17:23:37 -0400

    first commit

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
+words = ["x","y","z"]
+
 def oxford_comma(items):
-    return None
+    before_oxford = ", ".join(items[:len(items)-1])
+    after_oxford = before_oxford + " and " + items[-1]
+    return after_oxford
+
+
+print(oxford_comma(words))
